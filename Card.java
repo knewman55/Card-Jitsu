ebbbf4e64463bc6a5b25e633ca49ca684155d84b
Ken Newman <knewman55@gmail.com>
2017-01-14 02:26:35 +0000

    Card.java created online with Bitbucket

---
+++ Card.java
@@ -0,0 +1,32 @@
+package CardJitsu;
+
+public class Card {
+    private String Element;
+    private int ElementNum;
+    private int Strength;
+    private boolean CardDrawn;
+    
+    public Card(String elem, int elenum, int strength, boolean drawn){
+        this.Element=elem;
+        this.ElementNum=elenum;
+        this.Strength=strength;
+        this.CardDrawn=drawn;
+    }
+    public Card(){
+    }
+    public boolean drawn(){
+        return this.CardDrawn;
+    }
+    public void changeDrawn(){
+        this.CardDrawn=!this.CardDrawn;
+    }
+    public String getElement(){
+        return this.Element;
+    }
+    public int getElementNum(){
+        return this.ElementNum;
+    }
+    public int getStrength(){
+        return this.Strength;
+    }
+}
