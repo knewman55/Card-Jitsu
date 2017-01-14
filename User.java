1d628a2dbc6115aa84eedc08d8b6abc1956c10cf
Ken Newman <knewman55@gmail.com>
2017-01-14 02:29:53 +0000

    User.java created online with Bitbucket

---
+++ User.java
@@ -0,0 +1,28 @@
+package CardJitsu;
+
+import java.util.Scanner;
+
+public class User extends Player{
+    
+    public Card chooseCard(){
+        Card[][] deck = this.getDeck();
+        Scanner scan = new Scanner(System.in);
+        Card choice=null;
+               
+        while(choice == null){
+            try{
+                int userInput = scan.nextInt();
+                choice = deck[userInput - 1][0];
+            }catch(Exception e){
+                System.out.println("Choice out of range");
+            }
+        }
+        return choice;
+    }
+    public void printDeck(){
+        this.createDeck();
+        for(int i=0; i<this.getDeck().length; i++){
+            System.out.println(this.getDeck()[i][0].getElement() + " " + this.getDeck()[i][0].getStrength());
+        }
+    }
+}
