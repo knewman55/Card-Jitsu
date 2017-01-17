18dc7bd1f3a896438027bd9812c66e1dc9f43e72
Ken Newman <knewman55@gmail.com>
2017-01-17 04:13:15 +0000

    main.java edited online with Bitbucket

--- main.java
+++ main.java
@@ -1,23 +1,32 @@
 package CardJitsu;
 
-public class Main {
-    public static void main(String[] args) {
-        Dealer deal = new Dealer();
-        User use = new User();
-        Player play = null;
-        Logistics log = new Logistics();
-        
-        boolean[] initElementList = {false, false, false};
-        use.initElementList(initElementList);
-        deal.initElementList(initElementList);
-        
-        while(play == null){
-            use.printDeck();
-            Card userChoice = use.chooseCard();
-            Card dealerChoice = deal.chooseCard();
-            log.roundWinner(userChoice, dealerChoice, use, deal);
-            play = log.gameWinner(use, deal);
+import java.util.Scanner;
+
+public class User extends Player{
+    
+	public User(){
+		this.winDeclaration = "You have won the game!!!";
+	}
+    public Card chooseCard(){
+        Card[][] deck = this.getDeck();
+		Scanner scan = new Scanner(System.in);
+        Card choice=null;
+               
+        while(choice == null){
+            try{
+                int userInput = scan.nextInt();
+                choice = deck[userInput - 1][0];
+            }catch(Exception e){
+                System.out.println("Choice out of range");
+            }
         }
-        System.out.println("The " + play.name + " has won!");
+        System.out.println("\n\nYour Card: " + choice.getElement() + " " + choice.getStrength());
+        return choice;
+    }
+    public void printDeck(){
+        this.createDeck();
+        for(int i=0; i<this.getDeck().length; i++){
+            System.out.println(this.getDeck()[i][0].getElement() + " " + this.getDeck()[i][0].getStrength());
+        }
     }
 }
