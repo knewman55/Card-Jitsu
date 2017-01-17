a2d87432356088ba6f34d493abd4a1dd99c92930
Ken Newman <knewman55@gmail.com>
2017-01-17 04:15:28 +0000

    main.java edited online with Bitbucket

--- main.java
+++ main.java
@@ -1,32 +1,36 @@
 package CardJitsu;
 
-import java.util.Scanner;
-
-public class User extends Player{
-    
-	public User(){
-		this.winDeclaration = "You have won the game!!!";
-	}
-    public Card chooseCard(){
-        Card[][] deck = this.getDeck();
-		Scanner scan = new Scanner(System.in);
-        Card choice=null;
-               
-        while(choice == null){
-            try{
-                int userInput = scan.nextInt();
-                choice = deck[userInput - 1][0];
-            }catch(Exception e){
-                System.out.println("Choice out of range");
+public class Main {
+    public static void main(String[] args) {
+        Dealer deal = new Dealer();
+        User use = new User();
+        Player play = null;
+        Card winner;
+        Logistics log = new Logistics();
+        
+        boolean[] uinitElementList = {false, false, false};
+        boolean[] dinitElementList = {false, false, false};
+        use.initElementList(uinitElementList);
+        deal.initElementList(dinitElementList);
+        
+        System.out.println("Welcome to Card-Jitsu: Alpha 0.0");
+        while(play == null){
+        	System.out.println("\nYour Deck: ");
+            use.printDeck();
+            Card userChoice = use.chooseCard();
+            Card dealerChoice = deal.chooseCard();
+            winner = log.roundWinner(userChoice, dealerChoice, use, deal);
+            if(winner == userChoice){
+            	System.out.println("You won that round!");
+            }else if(winner == dealerChoice){
+            	System.out.println("Opponent won that round!");
+            }else{
+            	System.out.println("Tie!");
             }
+            play = log.gameWinner(use, deal);
+            System.out.println("You have : " + use.printElementList());
+            System.out.println("Opponent has : " + deal.printElementList());
         }
-        System.out.println("\n\nYour Card: " + choice.getElement() + " " + choice.getStrength());
-        return choice;
-    }
-    public void printDeck(){
-        this.createDeck();
-        for(int i=0; i<this.getDeck().length; i++){
-            System.out.println(this.getDeck()[i][0].getElement() + " " + this.getDeck()[i][0].getStrength());
-        }
+        System.out.println(play.getWinDeclaration());
     }
 }
